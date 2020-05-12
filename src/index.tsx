f056fcbd87a3f863570ba199a8f844bb1cfccc89
danicolms.c <=>
2020-05-12 17:39:28 +0200

    Boilerplate basico con react / parcel / typescript

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return <>📦</>;
+  }
+}
+
+render(<App />, document.getElementById("root"));
